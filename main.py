15afcc1f55a4fc341b73d5c4accd7d357e208fe6
CMadaleno <129568588+madxin@users.noreply.github.com>
2026-05-28 12:22:40 +0100

    update 2
    adicionada função de soma

--- main.py
+++ main.py
@@ -3,3 +3,10 @@ print("Olá")
 nome = input("Qual é o teu nome? ")
 
 print("Bem-Vindo,", nome)
+
+numero1 = float(input("Digite o primeiro número: "))
+numero2 = float(input("Digite o segundo número: "))
+
+soma = numero1 + numero2
+
+print("A soma é:", soma)
